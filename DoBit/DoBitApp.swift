5805d441826ee4f945e59e0c019185ab036779a0
KimWooSun <kipsong133@gmail.com>
2021-12-01 18:55:01 +0900

    Initial Commit

---
+++ DoBit/DoBitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoBitApp.swift
+//  DoBit
+//
+//  Created by 김우성 on 2021/12/01.
+//
+
+import SwiftUI
+
+@main
+struct DoBitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
